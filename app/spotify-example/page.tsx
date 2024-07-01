a5dc547f58bb00cc8241732196b0d525e70116df
Andrew Knox <andrew.knox.r@gmail.com>
2024-07-01 15:36:38 -0700

    added next.js example code and modified it a bit to work

---
+++ app/spotify-example/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { Artist, ItemTypes, SearchResults, SpotifyApi, Page, Track } from "@spotify/web-api-ts-sdk"; // use "@spotify/web-api-ts-sdk" in your own project
+import sdk from "@/lib/spotify-sdk/ClientInstance";
+import { useSession, signOut, signIn } from "next-auth/react";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const session = useSession();
+
+  if (!session || session.status !== "authenticated") {
+    return (
+      <div>
+        <h1>Spotify Web API Typescript SDK in Next.js</h1>
+        <button onClick={() => signIn("spotify")}>Sign in with Spotify</button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <p>Logged in as {session.data.user?.name}</p>
+      <button onClick={() => signOut()}>Sign out</button>
+      <SpotifySearch sdk={sdk} />
+    </div>
+  );
+}
+
+function SpotifySearch({ sdk }: { sdk: SpotifyApi }) {
+  const [results, setResults] = useState<SearchResults<ItemTypes[]>>({} as SearchResults<ItemTypes[]>);
+  const [userSongs, setUserSongs] = useState<Page<Track>>({} as Page<Track>);
+
+  useEffect(() => {
+    (async () => {
+      const res = await sdk.currentUser.topItems("tracks");
+      const results = await sdk.search("The Beatles", []);
+      setUserSongs(() => res);
+    })();
+  }, [sdk]);
+
+  console.log(userSongs);
+  // generate a table for the results
+  const tableRows = results.artists?.items.map((artist:Artist) => {
+    return (
+      <tr key={artist.id}>
+        <td>{artist.name}</td>
+        <td>{artist.popularity}</td>
+        <td>{artist.followers.total}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <>
+      <h1>Spotify Search for The Beatles</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Followers</th>
+          </tr>
+        </thead>
+        <tbody>{tableRows}</tbody>
+      </table>
+    </>
+  );
+}
