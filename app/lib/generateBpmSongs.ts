d01c82d3746da14a5a3da43065c50fb801197d72
Andrew Knox <andrew.knox.r@gmail.com>
2024-07-03 14:40:20 -0700

    added functionality

--- app/lib/generateBpmSongs.ts
+++ app/lib/generateBpmSongs.ts
@@ -1,5 +1,7 @@
 import { AuthSession, Track, TrackAnalysis } from "../types/types";
-import { getManyTrackAnalysis } from "./actions";
+import { getManyTrackAnalysis, getTrackFromPlaylistLink } from "./actions";
+import { getAllUserLikedPlaylists } from "./actions";
+import { Playlist } from "../types/types";
 
 function chunkArray<T>(array: T[], chunkSize: number): T[][] {
   const result: T[][] = [];
@@ -9,21 +11,30 @@ function chunkArray<T>(array: T[], chunkSize: number): T[][] {
   return result;
 }
 
-async function generateBpmSongs(lowBpm: number, highBpm: number, songs: Track[], session: AuthSession): Promise<Track[]> {
+async function generateBpmSongs(lowBpm: number, highBpm: number, session: AuthSession): Promise<Track[]> {
+  // get all users playlists
+  const userPlaylists = (await getAllUserLikedPlaylists(session).then(
+    (data) => data.sort((a, b) => a.name.localeCompare(b.name))
+  )) as Playlist[];
+
+  const songs = [];
+
+  // get all songs from the playlists
+  for (const playlist of userPlaylists.slice(3, 6)) {
+    const playlistTracks = await getTrackFromPlaylistLink(session, playlist.tracks.href)
+    songs.push(...playlistTracks.map(item => item.track));
+  }
+
   const chunkedSongs = chunkArray(songs, 100);
   const results: any[] = [];
 
   for (const chunk of chunkedSongs) {
     const features = await getManyTrackAnalysis(session, chunk.map(song => song.id));
-    results.push(...features);
+    console.log(features);
+    results.push(...features.filter(song => song.tempo !== null && song.tempo >= lowBpm && song.tempo <= highBpm));
   }
 
-  // Filter songs based on BPM or any other logic
-  const filteredSongs: Track[] = results.filter(song => {
-    return song.tempo >= lowBpm && song.tempo <= highBpm;
-  });
-
-  return filteredSongs;
+  return results.map(song => song.id);
 }
 
 export default generateBpmSongs;
