d83471a7750ca84da1ff1ee7fa630e6f83e406c4
Andrew Knox <andrew.knox.r@gmail.com>
2024-07-10 16:42:49 -0700

    adding a tooltip to the checkbox

--- app/components/Checkbox.tsx
+++ app/components/Checkbox.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface CheckboxProps {
   label: string;
   checked: boolean;
+  hint: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Checkbox: React.FC<CheckboxProps> = ({ label, checked, onChange }) => (
-  <div>
-    <label>
-      <input type="checkbox" checked={checked} onChange={onChange} />
-      {label}
-    </label>
-  </div>
-);
+const Checkbox: React.FC<CheckboxProps> = ({ label, checked, hint, onChange }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <div
+      className="relative inline-block"
+      onMouseOver={() => setIsHovered(true)}
+      onMouseOut={() => setIsHovered(false)}
+    >
+      <label className="flex items-center space-x-2">
+        <input type="checkbox" checked={checked} onChange={onChange} className="form-checkbox" />
+        <span>{label}</span>
+      </label>
+      {isHovered && (
+        <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-32 bg-gray-700 text-white text-center text-sm rounded-md py-1 opacity-90">
+          {hint}
+        </span>
+      )}
+    </div>
+  );
+};
 
 export default Checkbox;
