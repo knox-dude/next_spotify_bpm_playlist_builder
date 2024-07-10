e9ab49e103143c019c9838ea817aaf02a6c9893e
Andrew Knox <andrew.knox.r@gmail.com>
2024-07-10 16:43:24 -0700

    adding some styling

--- app/components/TextInput.tsx
+++ app/components/TextInput.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 interface TextInputProps {
   label: string;
   value: string;
+  placeholder?: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const TextInput: React.FC<TextInputProps> = ({ label, value, onChange }) => (
-  <div>
-    <label>{label}</label>
-    <input type="text" value={value} onChange={onChange} />
+const TextInput: React.FC<TextInputProps> = ({ label, value, placeholder, onChange }) => (
+  <div className=' items-center'>
+    <input className='text-black bg-gray-200 rounded-md m-2' placeholder={placeholder} name={label} type="text" value={value} onChange={onChange} />
   </div>
 );
 
