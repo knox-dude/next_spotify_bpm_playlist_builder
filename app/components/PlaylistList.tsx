e26a9ef54c85d6c21882f76fb82cb4cd0759d012
Andrew Knox <andrew.knox.r@gmail.com>
2024-07-11 10:05:49 -0700

    adding loading icons

--- app/components/PlaylistList.tsx
+++ app/components/PlaylistList.tsx
@@ -1,27 +1,47 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import PlaylistDisplay from './PlaylistDisplay';
+import { AuthSession, Playlist } from '../types/types';
+import { getAllUserLikedPlaylists } from '../lib/actions';
+import { Audio } from 'react-loader-spinner';
 
-const PlaylistList: React.FC = () => {
-  const playlists = [
-    { id: 1, name: 'Playlist 1asdfasdfasdfasdfasdfasdfasdf', imageUrl: '' },
-    { id: 2, name: 'Playlist 2', imageUrl: '' },
-    { id: 3, name: 'Playlist 3', imageUrl: '' },
-    { id: 4, name: 'Playlist 4', imageUrl: '' },
-    { id: 5, name: 'Playlist 5', imageUrl: '' },
-    { id: 6, name: 'Playlist 6', imageUrl: '' },
-    // Add more playlists here
-  ];
+
+function PlaylistList({session}:{session:AuthSession}) {
+
+  useEffect(() => {
+    async function getUserPlaylists() {
+      const userPlaylists = (await getAllUserLikedPlaylists(session).then(
+        (data) => data.sort((a, b) => a.name.localeCompare(b.name))
+      )) as Playlist[];
+      setPlaylists(userPlaylists);
+      setLoading(false);
+    }
+    getUserPlaylists();
+  }, [session])
+
+  const [loading, setLoading] = useState(true);
+  const [playlists, setPlaylists] = useState<Playlist[]>([]);
 
   return (
-    <div className="grid grid-cols-[repeat(auto-fit,_minmax(7.5rem,_1fr))] gap-4 w-full">
-      {playlists.map((playlist) => (
-        <PlaylistDisplay
-          key={playlist.id}
-          name={playlist.name}
-          imageUrl={playlist.imageUrl}
-        />
-      ))}
-    </div>
+    <>
+      {loading && (
+        <div className="flex flex-col justify-center items-center w-full h-full">
+          <Audio color="#1DB954" height={80} width={80} />
+          <p className='font-bold text-2xl text-gray-400'>Loading your playlists...</p>
+        </div>
+      
+      )}
+      {!loading && (
+        <div className="grid grid-cols-[repeat(auto-fit,_minmax(7.5rem,_1fr))] gap-4 w-full">
+          {playlists.map((playlist) => (
+            <PlaylistDisplay
+              key={playlist.id}
+              name={playlist.name}
+              imageUrl={playlist.images.length > 0 ? playlist.images[0].url : ""}
+            />
+          ))}
+        </div>
+      )}
+      </>
   );
 };
 
