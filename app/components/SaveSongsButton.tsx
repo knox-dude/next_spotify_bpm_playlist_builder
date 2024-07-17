aec0e4b540d382cd6613a2576627087f45fb1465
Andrew Knox <andrew.knox.r@gmail.com>
2024-07-17 16:53:35 -0700

    style change

--- app/components/SaveSongsButton.tsx
+++ app/components/SaveSongsButton.tsx
@@ -10,7 +10,7 @@ function SaveSongsButton({onClick}:{onClick:(songs:TrackWithAnalysis[]) => void}
     <button
     type="button"
     onClick={() => onClick(selectedSongs)}
-    className="bg-paper-500 mb-4 text-white rounded-md p-2 disabled:cursor-not-allowed disabled:opacity-30 enabled:hover:bg-paper-600"
+    className=" w-1/2 self-center bg-paper-500 mb-4 text-white rounded-md p-2 disabled:cursor-not-allowed disabled:opacity-30 enabled:hover:bg-paper-600"
   >
     Save Songs to Playlist
   </button>
