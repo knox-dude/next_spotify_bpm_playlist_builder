6c403794b9ce001f8ca5590014a45398d8e9d280
Andrew Knox <andrew.knox.r@gmail.com>
2024-07-17 16:54:04 -0700

    style change

--- app/components/TextInput.tsx
+++ app/components/TextInput.tsx
@@ -5,12 +5,18 @@ interface TextInputProps {
   value: string;
   placeholder?: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  className?: string;
 }
 
-const TextInput: React.FC<TextInputProps> = ({ label, value, placeholder, onChange }) => (
-  <div className=' items-center'>
-    <input className='text-black bg-gray-200 rounded-md m-2' placeholder={placeholder} name={label} type="text" value={value} onChange={onChange} />
-  </div>
+const TextInput: React.FC<TextInputProps> = ({ label, value, placeholder, onChange, className = '' }) => (
+    <input
+      className={`text-black bg-gray-200 rounded-md m-2 text-center p-1 ${className}`}
+      placeholder={placeholder}
+      name={label}
+      type="text"
+      value={value}
+      onChange={onChange}
+    />
 );
 
 export default TextInput;
