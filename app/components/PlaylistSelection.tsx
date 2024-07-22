5e5fe2ab50223f765077e18199865e56d4e0edad
Andrew Knox <andrew.knox.r@gmail.com>
2024-07-22 12:57:09 -0700

    adding more direction for users

--- app/components/PlaylistSelection.tsx
+++ app/components/PlaylistSelection.tsx
@@ -9,7 +9,7 @@ interface PlaylistSelectionProps {
 const PlaylistSelection: React.FC<PlaylistSelectionProps> = ({ session }) => (
   <>
     <div className="self-center flex justify-around w-11/12 text-gray-400 font-bold text-2xl mb-2">
-      <p> Choose playlists </p>
+      <p> Choose (click) playlists to use for the BPM song scan </p>
     </div>
     <PlaylistList session={session} />
   </>
