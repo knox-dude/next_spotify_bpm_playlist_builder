9c9a643ea50019fac1ec6274adfa9676e7bc5bb9
Andrew Knox <andrew.knox.r@gmail.com>
2024-07-21 23:06:23 -0700

    changing jest config a bit

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,15 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest', // Transform TypeScript files using ts-jest
+    '^.+\\.jsx?$': 'babel-jest', // Transform JavaScript files using babel-jest
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'], // Matches test files with .js, .jsx, .ts, .tsx extensions
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json', // Ensure you have a tsconfig.json file
+    },
+  },
 };
