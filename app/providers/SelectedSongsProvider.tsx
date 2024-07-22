be1288234194f12201b3229208a682005668c197
Andrew Knox <andrew.knox.r@gmail.com>
2024-07-22 12:07:44 -0700

    removing unused import

--- app/providers/SelectedSongsProvider.tsx
+++ app/providers/SelectedSongsProvider.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useContext, ReactNode } from 'react';
-import { Playlist, TrackWithAudioFeature } from '../types/updatedTypes';
+import { TrackWithAudioFeature } from '../types/updatedTypes';
 
 interface SelectedSongsContextProps {
   selectedSongs: TrackWithAudioFeature[];
