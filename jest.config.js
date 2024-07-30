581b432e5056643bfb2af8c6e30acad507110d2a
Andrew Knox <andrew.knox.r@gmail.com>
2024-07-30 14:31:51 +0200

    adding config for mocking of fetch

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,13 @@ const config = {
   testEnvironment: 'jsdom',
   // Add more setup options before each test is run
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^jose': require.resolve('jose'),
+    '^@panva/hkdf': require.resolve('@panva/hkdf'),
+    '^preact-render-to-string': require.resolve('preact-render-to-string'),
+    '^preact': require.resolve('preact'),
+    '^uuid': require.resolve('uuid'),
+  },
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,8 @@
 import '@testing-library/jest-dom';
+const { TextEncoder, TextDecoder } = require('util');
+Object.assign(global, { TextDecoder, TextEncoder });
+// Makes fetch available globally to prevent a Warning
+import 'whatwg-fetch';
+global.fetch = require('jest-fetch-mock');
+
+fetch.mockResponse(JSON.stringify({ testing: true })); // mock every fetchCall in all tests with this default value
